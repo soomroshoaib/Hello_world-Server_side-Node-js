ee7572de866f2d3c77f17d98b724c19b297da53a
shoaib soomro <64408582+soomroshoaib@users.noreply.github.com>
2022-08-10 00:40:59 -0700

    minor change

--- index.mjs
+++ index.mjs
@@ -1,10 +1,24 @@
 import express from "express"
-const app = express();
-const port = 3000;
+import cors from "cors"
 
-app.get('/',(req, res)=>{
-    res.send(" Soomro World 123444 soomro 123456789 ")
+
+const app = express();
+app.use(cors())
+
+
+app.get('/Porfile',(req, res)=>{
+    res.send(" Soomro World 123444 soomro 123456789 bjakakka ")
 });
+
+app.get('/information',(req, res)=>{
+    res.send({
+         Name: "Shoaib Soomro",
+         City:  "Karachi",
+         Distric: "Rahimyar Khan"
+    })
+});
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, ()=>{
     console.log(`Expample app listing on port ${port}`)
